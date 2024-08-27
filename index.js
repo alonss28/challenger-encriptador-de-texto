5238a657eaa51fd13778486d9d3fb3fe4cd6bb22
alonss28 <alonsojya7@gmail.com>
2024-08-27 17:01:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let lupa = document.getElementById("lupa");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value =textoCifrado;
+    parrafo.textContent = "";
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    
+    lupa.src = "./img/encriptado.png";
+  } else {
+    lupa.src = "./img/login.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("O000000h!", "Debes ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let lupa = document.getElementById("lupa");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      lupa.src = "./img/desencriptado.png";
+    } else {
+      lupa.src = "./img/login.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("O000000h!", "Debes ingresar un texto", "warning");
+    }
+}
